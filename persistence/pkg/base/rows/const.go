921776152055705d0b20398513b416ffd88513a3
romanovich <romanovich@yandex-team.com>
2026-07-22 15:32:37 +0300

    compression for ydbpgx commit_hash:757260856ba4eb1aa8252d1b3ab51df2e402b330

--- persistence/pkg/base/rows/const.go
+++ persistence/pkg/base/rows/const.go
@@ -11,3 +11,28 @@ const (
 	ItemTypeChasmNodeMetadata
 	ItemTypeChasmNodeData
 )
+
+func ItemTypeName(itemType int32) string {
+	switch itemType {
+	case ItemTypeActivity:
+		return "ActivityInfo"
+	case ItemTypeTimer:
+		return "TimerInfo"
+	case ItemTypeChildExecution:
+		return "ChildExecutionInfo"
+	case ItemTypeRequestCancel:
+		return "RequestCancelInfo"
+	case ItemTypeSignal:
+		return "SignalInfo"
+	case ItemTypeSignalRequested:
+		return "SignalRequested"
+	case ItemTypeChasmNodeMetadata:
+		return "ChasmNodeMetadata"
+	case ItemTypeChasmNodeData:
+		return "ChasmNodeData"
+	case ItemTypeBufferedEvent:
+		return "BufferedEvent"
+	default:
+		return "StateMapItem"
+	}
+}
